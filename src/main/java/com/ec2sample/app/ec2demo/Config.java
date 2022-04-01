0823fbf2eb9c472c5b7f78b033fa80874fd2b355
Thiri Yadanar <tryatanar@gmail.com>
2022-04-01 13:07:30 +0630

    modify issues

--- src/main/java/com/ec2sample/app/ec2demo/Config.java
+++ src/main/java/com/ec2sample/app/ec2demo/Config.java
@@ -2,6 +2,7 @@ package com.ec2sample.app.ec2demo;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
@@ -10,22 +11,34 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableJdbcRepositories(basePackages = "com.ec2sample.app.ec2demo")
 public class Config extends AbstractJdbcConfiguration{
 
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Value("${spring.datasource.username}")
+	private String userName;
+
+	@Value("${spring.datasource.password}")
+	private String password;
+
 	@Bean("dataSource")
     public DataSource mysqlDataSource() {
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/userdata");
-        dataSource.setUsername("root");
-        dataSource.setPassword("thiri2896");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
         return dataSource;
     }
+
+
 	@Bean
 	NamedParameterJdbcOperations namedParameterJdbcOperations(DataSource dataSource)
 	{
--- src/main/java/com/ec2sample/app/ec2demo/Ec2demoApplication.java
+++ src/main/java/com/ec2sample/app/ec2demo/Ec2demoApplication.java
@@ -3,8 +3,10 @@ package com.ec2sample.app.ec2demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class Ec2demoApplication {
 
 	public static void main(String[] args) {
